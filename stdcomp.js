b1b82ce1b598fdfd1e257d6aca4ccd3e5022e026
Taisuke Fukuno <fukuno@jig.jp>
2021-10-08 07:46:42 +0900

    support create with param className

--- stdcomp.js
+++ stdcomp.js
@@ -1,47 +1,50 @@
-export const cr = (tag, parent) => {
+export const create = (tag, parent, className) => {
   const c = document.createElement(tag);
   if (parent) {
     parent.appendChild(c);
+  }
+  if (className) {
+    c.className = className;
   }
   return c;
 };
 
 export const createButton = (s) => {
-  const c = cr("button");
+  const c = create("button");
   c.textContent = s;
   return c;
 };
 
 export const createInput = (s) => {
-  const c = cr("input");
+  const c = create("input");
   c.type = "text";
   c.value = s;
   return c;
 };
 
 export const createSelect = (ar) => {
-  const c = cr("select");
+  const c = create("select");
   for (const a of ar) {
-    const op = cr("option", c);
+    const op = create("option", c);
     op.textContent = a;
   }
   return c;
 };
 
 export const createTable = (ar) => {
-  const tbl = cr("table");
-  const tr = cr("tr", tbl);
+  const tbl = create("table");
+  const tr = create("tr", tbl);
   for (const a of ar[0]) {
-    cr("th", tr).textContent = a;
+    create("th", tr).textContent = a;
   }
   for (let i = 1; i < ar.length; i++) {
     const as = ar[i];
-    const tr = cr("tr", tbl);
+    const tr = create("tr", tbl);
     for (const a of as) {
       if (typeof a == "string") {
-        cr("td", tr).textContent = a;
+        create("td", tr).textContent = a;
       } else {
-        const c = cr("td", tr);
+        const c = create("td", tr);
         c.appendChild(a);
       }
     }
